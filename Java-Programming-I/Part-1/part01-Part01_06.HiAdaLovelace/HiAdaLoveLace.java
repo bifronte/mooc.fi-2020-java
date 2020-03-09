589633b080bc1e1a9e677a55881caad19f6e4ca8
bifronte <bifronte@tutanota.com>
2020-03-09 21:24:46 +0100

    Part 1

---
+++ Java-Programming-I/Part-1/part01-Part01_06.HiAdaLovelace/HiAdaLoveLace.java
@@ -0,0 +1,17 @@
+public class HiAdaLovelace {
+
+ 
+
+    public static void main(String[] args) {
+
+        String name = "Ada Lovelace";
+
+ 
+
+        System.out.println("Hi " + name + "!");
+
+ 
+
+    }
+
+}
